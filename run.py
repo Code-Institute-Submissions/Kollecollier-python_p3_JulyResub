c324a34d9f925803486a62f5b302f441367d0897
Kollecollier <kristoffer.collier@live.se>
2022-05-06 10:05:36 +0000

    Add welcome messege and if statment to determen if user wishes to play

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 print("Welcome to my computer game")
+
+play = input('Do you want to play? ')
+
+if play != 'yes':
+    quit()
+
+print("Okej, Let's play ")
+
+answer = input("What is 'RAM' short for? 1: Random access memory. 2: Raid access memory. 3: Running access memory?")
