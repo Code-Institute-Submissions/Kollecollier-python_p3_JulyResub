05a9cf9601d343daff949a4a9b9455b744900074
Kollecollier <kristoffer.collier@live.se>
2022-05-11 12:12:43 +0000

    Add def wrong to tidy code up more

--- run.py
+++ run.py
@@ -1,6 +1,20 @@
+def wrong():
+    print("-----------------------")
+    print("Sorry that was incorrect!")
+    print("-----------------------")
+    play = input("Try again?\n")
+    if play == 'yes':
+        print("-----------------------")
+        print("Okej you've got this! 😉")
+        start_quiz()
+    else:
+        print("To bad maybe next time?")
+        exit()
+
+
 def ask_to_play():
-    print('Welcome to my computer quiz')
-    play = input('Do you want to play? ')
+    print('Welcome to my computer quiz!\n')
+    play = input('Do you want to play? Yes or No ?\n').lower()
     if play == "yes":
         start_game()
     else:
@@ -10,22 +24,21 @@ def ask_to_play():
 
 def start_game():
     print("Okej! Let's play!")
+    print("-----------------------")
     start_quiz()
 
 
 def start_quiz():
     print("First question! ")
     print("-----------------------")
-    playing = input ("What is RAM short for?\n")
-    if playing == "central processing unit":
+    play = input("What is RAM short for?\n").lower()
+    if play == "random access memory":
+        print("-----------------------")
         print("That's correct! Well done 😊")
+        print("-----------------------")
+        correct_answer()
     else:
-        print("Incorrect, try again?")
-        play = input("yes or no? ")
-        if play == 'yes':
-            print("----------")
-            restart_quiz()
-            print("-------------")    
+        wrong()
 
 
 def restart_quiz():
@@ -33,4 +46,22 @@ def restart_quiz():
     start_quiz()
 
 
+def correct_answer():
+    play = input('Okej next question, What is GPU short for?\n').lower()
+    if play == "graphics processing unit":
+        print("-----------------------")
+        print("Correct, Good job! 😊 ")
+        print("-----------------------")
+        print("Now let's move on to the next question!")
+        print("-----------------------")
+        play = input("What is SSD short for?\n").lower()
+        if play == 'Solid State Drive':
+            print("-----------------------")
+            print('That is correct! 😊')
+            print("-----------------------")
+    else:
+        wrong()
+
+
 ask_to_play()
+wrong()
